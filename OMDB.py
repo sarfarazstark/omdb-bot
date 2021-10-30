1e4134108787cd3b8fba234ec74a0a76048c04c2
Arun <86429222+Arun-TG@users.noreply.github.com>
2021-10-31 00:02:29 +0530

    more details

--- OMDB.py
+++ OMDB.py
@@ -1,9 +1,12 @@
+#sheriyenna
+
 import os
 import requests
+from info import API_KEY
 
 def get_movie_info(movieTitle):
     url = 'http://www.omdbapi.com'
-    api_key = os.getenv('APIKEY')
+    api_key = API_KEY
     data = {'apikey':api_key,'t':movieTitle}
     response = requests.get(url,data).json()
 
@@ -13,9 +16,19 @@ def get_movie_info(movieTitle):
     movie_info = {}
     movie_info["title"] = response.get("Title")
     movie_info["year"] = response.get("Year")
-    movie_info["plot"] = response.get("Plot")
+    movie_info["release"] = response.get("Released")
+    movie_info["duration"] = response.get("Runtime")
+    movie_info["genre"] = response.get("Genre")
+    movie_info["director"] = response.get("Director")
+    movie_info["writer"] = response.get("Writer")
     movie_info["actors"] = response.get("Actors")
+    movie_info["plot"] = response.get("Plot")
+    movie_info["language"] = response.get("Language")
+    movie_info["country"] = response.get("Country")
     movie_info["ratings"] = response.get("Ratings")
     movie_info["imdb_rating"] = response.get("imdbRating")
+    movie_info["votes"] = response.get("imdbVotes")
+    movie_info["imdb_id"] = response.get("imdbID")
+    movie_info["pimage"] = response.get("Poster")
 
     return movie_info
